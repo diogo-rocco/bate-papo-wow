5959ef39d75363f62728d7e3e13b69be1602b0e1
Diogo Rocco <diogorocco12@gmail.com>
2021-08-30 18:36:08 -0300

    Adding comments and removing unnecessary imports

--- server/server.py
+++ server/server.py
@@ -1,13 +1,9 @@
-from rpyc import ForkingServer
 from rpyc.utils.server import ThreadedServer
-from rpyc.utils.server import OneShotServer
 from client_manager import ClientManager
-import select
-import sys
 
 PORTA = 5000
 
+#Inicializa a camada de gerenciamento dos clientes
 server = ThreadedServer(ClientManager, port= PORTA)
-#server = OneShotServer(ClientManager, port= PORTA)
 
 server.start()

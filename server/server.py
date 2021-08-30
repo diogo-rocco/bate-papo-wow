d30bc016585c944d0ecd0550839be72fb7e31847
Diogo Rocco <diogorocco12@gmail.com>
2021-08-30 19:03:50 -0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,9 +1,7 @@
 from rpyc.utils.server import ThreadedServer
 from client_manager import ClientManager
 
-PORTA = 5000
-
 #Inicializa a camada de gerenciamento dos clientes
-server = ThreadedServer(ClientManager, port= PORTA)
+server = ThreadedServer(ClientManager)
 
 server.start()
